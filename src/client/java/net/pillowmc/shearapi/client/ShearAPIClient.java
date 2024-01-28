6017c892d14eed69029c10a223a98a8314f5dd3a
黑嫖233 <heipiao233@outlook.com>
2024-01-28 16:54:11 +0800

    Update ShearAPIClient.java
    [ci skip]

--- src/client/java/net/pillowmc/shearapi/client/ShearAPIClient.java
+++ src/client/java/net/pillowmc/shearapi/client/ShearAPIClient.java
@@ -2,7 +2,7 @@ package net.pillowmc.shearapi.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ShearAPIClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
